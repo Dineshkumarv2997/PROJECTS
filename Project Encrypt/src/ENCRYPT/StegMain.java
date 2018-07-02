961bb6357ee91775e0d90ab550da3375876243fd
DINESH <dineshkumarv2997@gmail.com>
2018-07-02 21:42:52 +0530

    first commit

---
+++ Project Encrypt/src/ENCRYPT/StegMain.java
@@ -0,0 +1,20 @@
+package ENCRYPT;
+
+import ENCRYPT1.*;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mohit
+ */
+public class StegMain {
+    public static void main(String arg[]) {
+    StegNewJFrame a=new StegNewJFrame();
+    a.setVisible(true);
+    a.setLocationRelativeTo(null);
+    }
+}
